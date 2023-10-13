31fa11a6b8c9f8fe7be4fcd60962bb127e0ebf14
marcin sitko <marcin.sitko@nokia.com>
2023-10-13 11:57:40 +0200

    Reformat Assigment3

--- Assignment3/exercise3.py
+++ Assignment3/exercise3.py
@@ -1,108 +1,41 @@
-# --------ZADANIE 3_1-----------
-
-# Ten kod jest poprawny składniowo
-#           x = 2
-#           y = 3
-#           if x > y:
-#               result = x
-#           else:
-#               result = y
-
-# Ten kod także jest poprawny składowniowo
-#           for i in "axby": print(ord(i) if ord(i) < 100 else i)
-
-# Tutaj print(i) powinno byc przez instrukcja warunkową if , dodatkowo nie ma instrukcji else
-#           for i in "axby": if ord(i) < 100 print(i)
-
-
-# -------------ZADANIE 3_2------------
-
-# Tutaj mamy tylko dwie zmienne, x i y natomiast chcemy im przypisac az 3 liczby
-# KOD
-#       x, y = 1, 2, 3
-
-# przypisanie nie tworzy listy tylko tuple tzn : X=(1,2,3)
-# tuple jest unmutable to znaczy ze nie mozemy zmieniac jej zawartosci dlatego ponizsza akcja nie moze zostac wykonana
-# KOD
-#     X = 1, 2, 3
-#     X[1] = 4
-
-
-# X=[1,2,3] , X[3] = 4 jest jak najbrdziej poprawne natomiast, natomiast string takze jest unmutable w rozumieniu jako tablica
-# chyba ze uzyjemy metody .split() a potem ''.join() zamiast X.append("d") mozemy wykonac X+="d" otrzymamy wtedy 'abcd'
-# #KOD
-#           X = [1, 2, 3]
-#           X[3] = 4
-#           X = "abc"
-#           X.append("d")
-
-# Poprawniony kod
-#           X=[1,2,3]
-#           X[3] = 4
-#           X="abc"
-#           X+='d'
-
-
-# Tutaj bledem jest przypisanie L=L.sort() z racji ze .sort() zwraca typ None potem mapuje typ None a nie liste
-# nalezy uzyc tutaj zamiast L=L.sort() -> L.sort() wtedy nasza lista jest zmodyfikowanai zachownay jest jej typ
-# dodatkowo lazey zmienic konstrukcje samej metody map() z L = list(map(pow, range(8))) na L = list(map(pow, L, range(8)))
-# poniewaz nie podajemy na jakiej liscie ma byc to wykonane tylko przypisujemy wynik do naszje listy
-# dodatkowo funkcja range(int) zaczyna liczenie od 0 takze pierwszy element naszej listy bedzie równy 1
-# KOD
-#           L = [3, 5, 4]
-#           L = L.sort()
-#           L = list(map(pow, range(8)))
-# Poprawiony KOD
-#            L=[3,5,4]
-#           L.sort()
-#           L = list(map(pow,L,range(1,8)))
-#           lub mozemy uzyc range(8) jezeli faktycznie chcemy zaczynac potegowanie od 0
-
-
-def exc_3_3():
+def exercise_3_3():
     for i in range(31):
         if i % 3 == 0:
             continue
         print(i)
 
 
-def exc_3_4():
-    print("\nOBLICZANIE TRZECIEJ POTEGI LICZBY RZECZYWISTEJ\n")
+def exercise_3_4():
+    print("\nCALCULATING THE CUBE OF A REAL NUMBER\n")
     while True:
-        user_input = input("Podaj liczbe: ")
+        user_input = input("Enter a number: ")
         if user_input == "stop":
             break
         if user_input.isnumeric():
             result = pow(float(user_input), 3)
-            print(
-                f"Twoja liczba {user_input} podniesiona do trzeciej potegi wynosi {result}"
-            )
+            print(f"Your number {user_input} raised to the third power is {result}")
         else:
-            print(f"\nNie podales liczby rzeczywistej tylko '{user_input}'")
+            print(f"\nYou didn't enter a real number; you entered '{user_input}'")
 
 
-def exc_3_5(lenght: int) -> str:
+# Jezeli dlugosc liczby na miarce sie zwieksza, trzeba zadbac o to by odleglosci pomiedzy kolejnymi liczbami byly mniejsze
+# PRZYKLAD -  0    1    2 ...  (4 spacje odstępu)   10   11   12 ...  (3 spacje)  100  101  102 ... (2 spacje)
+# Wtedy liczba zawsze bedzie znajdowac sie po znakiem "|"
+# oczywiscie to wszystko przy zalozeniu ze linijka posiada 4 znaki '.' oraz dwa znaki '|' w kazdym segmencie przy innych zalozeniach trzeba dostosowac zmienna offset
+def exercise_3_5(length: int) -> str:
     result = ""
-
-    # konstruowanie struktury miarki
     unit = "|...."
-    for i in range(lenght):
+    for i in range(length):
         result += unit
     result += "|"
 
     spaces = {}
-    off_set = 4
+    offset = 4
 
-    # Jezeli dlugosc liczby na miarce sie zwieksza, trzeba zadbac o to by odleglosci pomiedzy kolejnymi liczbami byly mniejsze
-    # PRZYKLAD -  0    1    2 ...  (4 spacje odstępu)   10   11   12 ...  (3 spacje)  100  101  102 ... (2 spacje)
-    # Wtedy liczba zawsze bedzie znajdowac sie po znakiem "|"
-
-    # oczywiscie to wszystko przy zalozeniu ze linijka posiada 4 znaki '.' oraz dwa znaki '|' w kazdym segmencie przy innych zalozeniach trzeba dostosowac zmienna offset
-
-    for i in range(lenght + 1):
+    for i in range(length + 1):
         if len(str(i)) > len(str(i - 1)):
-            off_set -= 1
-        spaces[i] = off_set
+            offset -= 1
+        spaces[i] = offset
 
     numbers = ""
     for k, v in spaces.items():
@@ -110,11 +43,10 @@ def exc_3_5(lenght: int) -> str:
 
     result += "\n" + numbers
 
-    # print(spaces)
     return result
 
 
-def exc_3_6(height: int, width: int) -> str:
+def exercise_3_6(height: int, width: int) -> str:
     line = "+----"
     pipes = "|    "
 
@@ -132,10 +64,7 @@ def exc_3_6(height: int, width: int) -> str:
     return result
 
 
-# exc_3_6(3,4)
-
-
-def exc_3_8(list_a, list_b):
+def exercise_3_8(list_a, list_b):
     list_a = set(list_a)
     list_b = set(list_b)
     a = list_a.intersection(list_b)
@@ -144,20 +73,14 @@ def exc_3_8(list_a, list_b):
     return list(a), list(b)
 
 
-# print(exc_3_8([1,2,3,4],[3,4,6,7,7]))
-
-
-def exc_3_9(l):
+def exercise_3_9(lst):
     result = []
-    for seq in l:
+    for seq in lst:
         result.append(sum(seq))
     return result
 
 
-# print(exc_3_9([[],[4],(1,2),[3,4],(5,6,7)]))
-
-
-def exc_3_10(input_user: str):
+def exercise_3_10(input_user: str):
     roman_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
     arabic_numeral = 0
@@ -170,6 +93,3 @@ def exc_3_10(input_user: str):
             arabic_numeral += value
         prev_value = value
     return arabic_numeral
-
-
-# print(exc_3_10("MIV"))
---
+++ Assignment3/tests.py
@@ -0,0 +1,51 @@
+import unittest
+
+# Import the functions you want to test
+from exercise3 import *
+
+
+class Test_exercises(unittest.TestCase):
+    def test_modulo_3_3(self):
+        print("\nPrinting 0-30 number without numbers divisible by 3")
+        exercise_3_3()
+
+    def test_cube(self):
+        print("\nEnter number to get cubic number, enter 'stop' to stop")
+        exercise_3_4()
+
+    def test_ruler(self):
+        print("\nPrinting Ruler Examples")
+        print(exercise_3_5(10))
+        print(exercise_3_5(5))
+        print(exercise_3_5(15))
+        print(exercise_3_5(20))
+
+    def test_grid(self):
+        print("\nPrinting Grid Examples")
+        print(exercise_3_6(2, 4))
+        print(exercise_3_6(2, 5))
+        print(exercise_3_6(3, 2))
+
+    def test_sum_of_lists(self):
+        self.assertEqual(
+            exercise_3_9([[], [4], (1, 2), [3, 4], (5, 6, 7)]), [0, 4, 3, 7, 18]
+        )
+        self.assertEqual(exercise_3_9([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [6, 15, 24])
+
+    def test_roman_to_arabic(self):
+        self.assertEqual(exercise_3_10("MIV"), 1004)
+        self.assertEqual(exercise_3_10("XCIV"), 94)
+        self.assertEqual(exercise_3_10("XXI"), 21)
+
+    def test_intersection_3_8(self):
+        intersection, union = exercise_3_8([1, 2, 3, 4], [3, 4, 6, 7, 7])
+        self.assertEqual(intersection, [3, 4])
+        self.assertEqual(union, [1, 2, 3, 4, 6, 7])
+
+        intersection, union = exercise_3_8([1, 2, 3], [4, 5, 6, 3])
+        self.assertEqual(intersection, [3])
+        self.assertEqual(union, [1, 2, 3, 4, 5, 6])
+
+
+if __name__ == "__main__":
+    unittest.main()
