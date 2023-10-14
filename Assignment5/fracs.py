48aad24aea909da9f1927c8f2030dd0e8ebf3cd8
idontfeelsogoood <msitko21@interia.pl>
2023-10-14 19:39:41 +0200

    add Assingment5 with unitests

---
+++ Assignment5/fracs.py
@@ -0,0 +1,73 @@
+from typing import List
+from math import gcd
+
+# frac - [numerator, denominator]
+
+def add_frac(frac1: List[int], frac2: List[int]):
+    common_den = frac1[1] * frac2[1]
+    result_numerator = (frac1[0]*frac2[1]) + (frac2[0]*frac1[1])
+    greates_common_divisor = gcd(result_numerator, common_den)
+    if common_den == 0:
+        return "unknown"
+    elif result_numerator == 0:
+        return 0
+    return [result_numerator // greates_common_divisor, common_den // greates_common_divisor]
+
+def sub_frac(frac1: List[int], frac2: List[int]):
+    common_den = frac1[1] * frac2[1]
+    result_numerator = (frac1[0]*frac2[1]) - (frac2[0]*frac1[1])
+    greates_common_divisor = gcd(result_numerator, common_den)
+    if common_den == 0:
+        return "unknown"
+    elif result_numerator == 0:
+        return 0
+    return [result_numerator // greates_common_divisor, common_den // greates_common_divisor]
+
+def mul_frac(frac1: List[int], frac2: List[int]):
+    result_numerator = frac1[0] * frac2[0]
+    result_den = frac1[1] * frac2[1]
+    greates_common_divisor = gcd(result_numerator, result_den)
+    if result_den == 0:
+        return "unknown"
+    elif result_numerator == 0:
+        return 0
+    return [result_numerator // greates_common_divisor, result_den //greates_common_divisor]
+
+def div_frac(frac1: List[int], frac2: List[int]):
+    result_numerator = frac1[0] * frac2[1]
+    result_den = frac1[1] * frac2[0]
+    greates_common_divisor = gcd(result_numerator, result_den)
+    if result_den == 0:
+        return "unknown"
+    elif result_numerator == 0:
+        return 0
+    return [result_numerator // greates_common_divisor, result_den //greates_common_divisor]
+
+def is_positive(frac: List[int]) -> bool:
+    return (frac[0]) > 0 and (frac[1] >0)
+
+def is_zero(frac: List[int]) -> bool:
+    return (frac[0] == 0)
+
+
+def cmp_frac(frac1: List[int], frac2: List[int]):
+    frac_1 = frac1[0] * frac2[1]
+    frac_2 = frac2[0] * frac1[1]
+
+    if frac_1 > frac_2:
+        return -1
+    elif frac_1 < frac_2:
+        return 1
+    else:
+        return 0
+
+def frac2float(frac: List[int]):
+    return frac[0] / frac[1]
+
+# print(add_frac([1,9],[3,27]))
+# print(sub_frac([1,9],[3,27]))
+# print(sub_frac([1, 17], [81, 27]))
+# print(mul_frac([0,1],[3,27]))
+# print(div_frac([1,9],[3,27]))
+# print(cmp_frac([1,27],[1,81]))
+# print(frac2float([10,4]))
---
+++ Assignment5/tests.py
@@ -0,0 +1,54 @@
+import unittest
+from fracs import *
+
+class TestFracs(unittest.TestCase):
+
+    def test_add_frac(self):
+        self.assertEqual(add_frac([1,9],[3,27]), [2, 9])
+        self.assertEqual(add_frac([1, 3], [3, 27]), [4, 9])
+        self.assertEqual(add_frac([1, 17], [81, 27]), [52, 17])
+        self.assertEqual(add_frac([0, 17], [0, 27]), 0)
+        self.assertEqual(add_frac([1, 0], [81, 27]), "unknown")
+
+    def test_sub_frac(self):
+        self.assertEqual(sub_frac([1,9],[3,27]), 0)
+        self.assertEqual(sub_frac([1, 17], [81, 27]), [-50,17])
+        self.assertEqual(sub_frac([0, 9], [3, 27]), [-1, 9])
+        self.assertEqual(sub_frac([1, 0], [3, 0]), "unknown")
+        self.assertEqual(sub_frac([1, 0], [3, 27]), "unknown")
+
+    def test_mul_frac(self):
+        self.assertEqual(mul_frac([1,9], [3,27]), [1, 81])
+        self.assertEqual(mul_frac([1,1], [3,3]), [1,1])
+        self.assertEqual(mul_frac([0, 1], [3, 3]), 0)
+        self.assertEqual(mul_frac([1, 0], [3, 3]), "unknown")
+
+    def test_div_frac(self):
+        self.assertEqual(div_frac([1,9], [3,27]), [1, 1])
+        self.assertEqual(div_frac([1, 1], [3,3]), [1, 1])
+        self.assertEqual(div_frac([0, 9], [3, 27]), 0)
+        self.assertEqual(div_frac([3, 0], [3, 27]), "unknown")
+
+    def test_cmp_frac(self):
+        self.assertEqual(cmp_frac([1, 27], [1,81]), -1)
+        self.assertEqual(cmp_frac([1, 2], [1, 81]), -1)
+        self.assertEqual(cmp_frac([1, 3], [1, 4]), -1)
+        self.assertEqual(cmp_frac([1, 27], [133, 81]), 1)
+        self.assertEqual(cmp_frac([1, 1], [3, 3]), 0)
+
+    def test_frac2float(self):
+        self.assertEqual(frac2float([6,3]), 2.0)
+        self.assertEqual(frac2float([20, 3]), 6.666666666666667)
+        self.assertEqual(frac2float([10, 4]), 2.5)
+
+    def test_is_positive(self):
+        self.assertTrue(is_positive([1, 2]))
+        self.assertFalse(is_positive([-1, 2]))
+        self.assertFalse(is_positive([1, -2]))
+
+    def test_is_zero(self):
+        self.assertTrue(is_zero([0, 1]))
+        self.assertFalse(is_zero([1, 2]))
+
+if __name__ == '__main__':
+    unittest.main()
