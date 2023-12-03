00b9fb975bf07b80b8e3fb7daac7df803382ef5b
idontfeelsogoood <msitko21@interia.pl>
2023-12-03 11:47:40 +0100

    add make4

--- Assignment7/rectangles.py
+++ Assignment7/rectangles.py
@@ -3,51 +3,47 @@ from points import Point
 
 class Rectangle:
     def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
         self.point_1 = Point(x1, y1)
         self.point_2 = Point(x2, y2)
 
-    def __str__(self):
-        return f"[{self.point_1.__str__()}, {self.point_2.__str__()}]"
+    def __str__(self) -> str:
+        return f"[{str(self.point_1)}, {str(self.point_2)}]"
 
-    def __repr__(self):
-        return f"Rectangle({self.x1}, {self.y1}, {self.x2}, {self.y2})"
+    def __repr__(self) -> str:
+        return f"Rectangle({self.point_1.x}, {self.point_1.y}, {self.point_2.x}, {self.point_2.y})"
 
-    def __eq__(self, other: "Rectangle"):
+    def __eq__(self, other: "Rectangle") -> bool:
         return (
-            (self.x1 == other.x1)
-            and (self.y1 == other.y1)
-            and (self.x2 == other.x2)
-            and (self.y2 == other.y2)
+            (self.point_1.x == other.point_1.x)
+            and (self.point_1.y == other.point_1.y)
+            and (self.point_2.x == other.point_2.x)
+            and (self.point_2.y == other.point_2.y)
         )
 
-    def __ne__(self, other: "Rectangle"):
+    def __ne__(self, other: "Rectangle") -> bool:
         return (
-            (self.x1 != other.x1)
-            or (self.y1 != other.y1)
-            or (self.x2 != other.x2)
-            or (self.y2 != other.y2)
+            (self.point_1.x != other.point_1.x)
+            or (self.point_1.y != other.point_1.y)
+            or (self.point_2.x != other.point_2.x)
+            or (self.point_2.y != other.point_2.y)
         )
 
-    def center(self):
-        x = (self.x1 + self.x2) / 2
-        y = (self.y1 + self.y2) / 2
+    def center(self) -> "Point":
+        x = (self.point_1.x + self.point_2.x) / 2
+        y = (self.point_1.y + self.point_2.y) / 2
         return Point(x, y)
 
-    def area(self):
+    def area(self) -> float:
         # Bo wiemy że x1 < x2, y1 < y2
-        x = self.x2 - self.x1
-        y = self.y2 - self.y1
+        x = self.point_2.x - self.point_1.x
+        y = self.point_2.y - self.point_1.y
         return x * y
 
-    def move(self, x, y):
+    def move(self, x, y) -> "Rectangle":
         moving = Point(x, y)
-        self.point_1 = self.point_1 + moving
-        self.point_2 = self.point_2 + moving
-        return f"[{self.point_1}, {self.point_2}]"
+        point_1 = self.point_1 + moving
+        point_2 = self.point_2 + moving
+        return Rectangle(point_1.x, point_1.y, point_2.x, point_2.y)
 
     def intersection(self, other: "Rectangle"):
         if (
@@ -70,12 +66,27 @@ class Rectangle:
         new_y_2 = max(self.point_2.y, other.point_2.y)
         return Rectangle(new_x_1, new_y_1, new_x_2, new_y_2)
 
-    # TODO:
-    # make4
+
+    def make4(self):
+        center = self.center()
+        x_ab = Point(center.x, self.point_1.y)
+        x_dc = Point(center.x, self.point_2.y)
+        y_da = Point(self.point_1.x, center.y)
+        y_cb = Point(self.point_2.x, center.y)
+        return (Rectangle(self.point_1.x,self.point_1.y,center.x, center.y),
+                Rectangle(y_da.x, y_da.y, x_dc.x, x_dc.y),
+                Rectangle(center.x, center.y, self.point_2.x, self.point_2.y),
+                Rectangle(x_ab.x, x_ab.y, y_cb.x, y_cb.y))
+
+
+
+
 
 
 rec1 = Rectangle(1, 0, 3, 3)
-rec2 = Rectangle(1, 3, 2, 3)
-print(rec1 == rec2)
-print(rec1.center())
-print(rec1.area())
+print(rec1.make4())
+
+# rec2 = Rectangle(1, 3, 2, 3)
+# print(rec1 == rec2)
+# print(rec1.center())
+# print(rec1.area())
